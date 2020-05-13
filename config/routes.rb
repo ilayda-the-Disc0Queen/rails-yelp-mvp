1eb38b81f4eced92d82a6cac7b17f943c3ac2850
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-05-13 16:36:43 +0100

    added some methods to restaturants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [:new, :create, :destroy] # needs destroy here?
-  end
+  # resources :restaurants do
+  #   resources :reviews, only: [:new, :create, :destroy] # needs destroy here?
+  # end
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:index, :new, :create]
+end
+
+  # get "/restaurants", to: "restaurants#index"
+  # get "/restaurants/new", to: "restaurants#new"
+  # post "/restaurants/:id", to: "restaurants#create"
+  # get "/restaurants/:id", to: "restaurants#show"
 end
